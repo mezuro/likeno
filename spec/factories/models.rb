71d28bd7dec120ce41dd9bf3405d9bc785ff3cb7
Heitor Reis <marcheing@gmail.com>
2015-12-15 09:38:50 -0200

    Configure FactgoryGirl on spec_helper
    Also added a generic model that inherits from Likeno::Base.

---
+++ spec/factories/models.rb
@@ -0,0 +1,4 @@
+FactoryGirl.define do
+  factory :model, class: Likeno::Base do
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,10 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'likeno'
+require 'factory_girl'
+
+FactoryGirl.find_definitions
 
 RSpec.configure do |config|
   config.mock_with :mocha
+  config.include FactoryGirl::Syntax::Methods
 end
